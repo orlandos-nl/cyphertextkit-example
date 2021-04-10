72eca6f37bf50b05a69b7b879593a5845c959a0d
Joannis orlandos <joannis@orlandos.nl>
2021-04-11 00:42:26 +0200

    Initial Commit

---
+++ Workspaces/WorkspacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkspacesApp.swift
+//  Workspaces
+//
+//  Created by Joannis Orlandos on 11/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct WorkspacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
